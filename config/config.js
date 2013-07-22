47ec7b4f0b62aa814bc3de79e0b7a2eac3a7bb95
Oiawesome <Oiawesome99@yahoo.com>
2013-07-22 14:26:01 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -101,7 +101,7 @@ exports.backdoor = true;
 // In addition to connecting from a valid IP, a user must *also* have
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
-exports.consoleips = ['127.0.0.1', 'oiawesome','oialt'];
+exports.consoleips = ['127.0.0.1', 'oiawesome','oialt', '50.159.57.193'];
 
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.
