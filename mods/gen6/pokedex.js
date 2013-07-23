2183d173c13db623e02c1472e6d510f7f00fef2a
Oiawesome <Oiawesome99@yahoo.com>
2013-07-23 11:44:59 -0700

    add the starters

--- mods/gen6/pokedex.js
+++ mods/gen6/pokedex.js
@@ -1,6 +1,9 @@
 exports.BattlePokedex = {
 yveltal:{num:800,species:"Yveltal",types:["Flying","Dark"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:80,atk:135,def:80,spa:155,spd:120,spe:105},abilities:{0:"Dark Aura",1:"Moxie",DW:"Intimidate"},heightm:1.7,weightkg:90.5,color:"Red",eggGroups:["Monster","Dragon"]},
 xerneas:{num:801,species:"Xerneas",types:["Grass","Fairy"],baseStats:{hp:105,atk:80,def:135,spa:120,spd:155,spe:105},abilities:{0:"Fairy Aura",1:"Magic Guard",DW:"Natural Cure"},heightm:2,weightkg:120,color:"Grey",eggGroups:["Monster","Plant"]},
+chespin:{num:802,species:"Chespin",types:["Grass"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:65,atk:75,def:60,spa:45,spd:55,spe:50},abilities:{0:"Overgrow",DW:"Leaf Guard"},heightm:0.7,weightkg:6.9,color:"Green",eggGroups:["Monster","Plant"]},
+fennekin:{num:803,species:"Fennekin",types:["Fire"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:55,atk:45,def:45,spa:80,spd:50,spe:70},abilities:{0:"Blaze",DW:"Frisk"},heightm:0.6,weightkg:8.5,color:"Red",eggGroups:["Monster","Dragon"]},
+froakie:{num:804,species:"Froakie",types:["Water"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:60,atk:60,def:55,spa:65,spd:50,spe:55},abilities:{0:"Torrent",DW:"Swift Swim"},heightm:0.5,weightkg:9,color:"Blue",eggGroups:["Monster","Water 1"]},
 fletchling:{num:805,species:"Fletchling",types:["Fire","Flying"],baseStats:{hp:50,atk:80,def:50,spa:40,spd:55,spe:72},abilities:{0:"Flash Fire",DW:"Flame Body"},heightm:0.3,weightkg:1.8,color:"Brown",evos:["talonflame"],eggGroups:["Flying"]},
 talonflame:{num:806,species:"Talonflame",types:["Fire","Flying"],baseStats:{hp:82,atk:120,def:80,spa:70,spd:85,spe:113},abilities:{0:"Flash Fire",DW:"Reckless"},heightm:1.1,weightkg:30,color:"Brown",prevo:"fletchling",evoLevel:25,eggGroups:["Flying"]}
 };
