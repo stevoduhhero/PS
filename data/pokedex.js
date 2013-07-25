8a037dc19df700da25398e4c86a1239d5d631f90
Oiawesome <Oiawesome99@yahoo.com>
2013-07-24 22:10:12 -0700

    Update pokedex.js

--- data/pokedex.js
+++ data/pokedex.js
@@ -712,8 +712,4 @@ cyclohm:{num:-58,species:"Cyclohm",types:["Electric","Dragon"],baseStats:{hp:108
 colossoil:{num:-59,species:"Colossoil",types:["Dark","Ground"],baseStats:{hp:133,atk:122,def:72,spa:71,spd:72,spe:95},abilities:{0:"Rebound",1:"Guts"},heightm:2.6,weightkg:683.6,color:"",eggGroups:["Water 2","Ground"]},
 krilowatt:{num:-60,species:"Krilowatt",types:["Electric","Water"],baseStats:{hp:151,atk:84,def:73,spa:83,spd:74,spe:105},abilities:{0:"Trace",1:"Magic Guard"},heightm:0.7,weightkg:10.6,color:"",eggGroups:["Water 1","Fairy"]},
 voodoom:{num:-61,species:"Voodoom",types:["Fighting","Dark"],baseStats:{hp:90,atk:85,def:80,spa:105,spd:80,spe:110},abilities:{0:"Volt Absorb",1:"Lightningrod"},heightm:2,weightkg:75.5,color:"",eggGroups:["Humanshape","Ground"]}
-yveltal:{num:800,species:"Yveltal",types:["Flying","Dark"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:80,atk:135,def:80,spa:155,spd:120,spe:105},abilities:{0:"Dark Aura",1:"Moxie",DW:"Intimidate"},heightm:1.7,weightkg:90.5,color:"Red",eggGroups:["Monster","Dragon"]},
-xerneas:{num:801,species:"Xerneas",types:["Grass","Fairy"],baseStats:{hp:105,atk:80,def:135,spa:120,spd:155,spe:105},abilities:{0:"Fairy Aura",1:"Magic Guard",DW:"Natural Cure"},heightm:2,weightkg:120,color:"Grey",eggGroups:["Monster","Plant"]},
-fletchling:{num:805,species:"Fletchling",types:["Fire","Flying"],baseStats:{hp:50,atk:80,def:50,spa:40,spd:55,spe:72},abilities:{0:"Flash Fire",DW:"Flame Body"},heightm:0.3,weightkg:1.8,color:"Brown",evos:["talonflame"],eggGroups:["Flying"]},
-talonflame:{num:806,species:"Talonflame",types:["Fire","Flying"],baseStats:{hp:82,atk:120,def:80,spa:70,spd:85,spe:113},abilities:{0:"Flash Fire",DW:"Reckless"},heightm:1.1,weightkg:30,color:"Brown",prevo:"fletchling",evoLevel:25,eggGroups:["Flying"]}
 };
