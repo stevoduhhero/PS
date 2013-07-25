8fc70c43ae82656b956cba19b574c4ea4309d8e2
Oiawesome <Oiawesome99@yahoo.com>
2013-07-24 21:41:10 -0700

    Update scripts.js

--- mods/gen6/scripts.js
+++ mods/gen6/scripts.js
@@ -25,37 +25,69 @@ exports.BattleScripts = {
 
 		//Brave Bird Is now a tutor move		
 	this.modData('Learnsets', 'charizard').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'butterfree').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'fearow').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'dodrio').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'gyarados').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'aerodactyl').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'articuno').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'zapdos').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'moltres').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'dragonite').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'noctowl').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'xatu').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'gligar').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'gliscor').learnset.bravebird = ['5T'];
+	
 	this.modData('Learnsets', 'lugia').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'peliper').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'masquerain').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'ninjask').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'salamence').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'rayquaza').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'chatot').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'togekiss').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'arceus').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'mew').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'pidove').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'tranquill').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'unfezant').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'archen').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'archeops').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'swanna').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'tornadus').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'thundurus').learnset.bravebird = ['5L0'];
+	
 	this.modData('Learnsets', 'landorus').learnset.bravebird = ['5L0'];
 
 		// Every DW ability that isn't Shadow Tag becomes released
