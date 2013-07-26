08e1588cca7cfc9e333cfab89c7ea1914f0a682a
stevoduhhero <Serditech@Valeria.vale>
2013-07-26 14:53:54 -0700

    git commit -a

--- mods/pokecybertiers/scripts.js
+++
@@ -1,6 +0,0 @@
-exports.BattleScripts = {
-        init: function() {
-                for (var i in this.data.FormatsData)
-                        this.data.FormatsData[i].dreamWorldRelease = true;
-                }
-};
