70486bf9bda4b53e346bd5c2690e8f68679c80c1
Oiawesome <Oiawesome99@yahoo.com>
2013-07-28 12:45:05 -0700

    Update items.js

--- mods/popculturemons/items.js
+++ mods/popculturemons/items.js
@@ -1,1 +1,101 @@
-gjhjlk
+exports.BattleItems = {
+  "burndrive": {
+		id: "burndrive",
+		name: "Burn Drive",
+		spritenum: 103,
+		fling: {
+			basePower: 70
+		},
+		onStart: function (pokemon) {
+			if (pokemon.species === 'Genesect') {
+				this.add('-item', pokemon, 'Burn Drive');
+				if (pokemon.formeChange('Genesect-Burn')) {
+					this.add('-formechange', pokemon, 'Genesect-Burn');
+				}
+			}
+		},
+		onBasePower: function(basePower, user, target, move) {
+			if (move.id === 'technoblast') {
+				return basePower * 1.1;
+			}
+		},
+		onDrive: 'Fire',
+		desc: "Changes Genesect to Genesect-Burn."
+	},
+	"chilldrive": {
+		id: "chilldrive",
+		name: "Chill Drive",
+		spritenum: 103,
+		fling: {
+			basePower: 70
+		},
+		onStart: function (pokemon) {
+			if (pokemon.species === 'Genesect') {
+				this.add('-item', pokemon, 'Chill Drive');
+				if (pokemon.formeChange('Genesect-Chill')) {
+					this.add('-formechange', pokemon, 'Genesect-Chill');
+				}
+			}
+		},
+		onBasePower: function(basePower, user, target, move) {
+			if (move.id === 'technoblast') {
+				return basePower * 1.1;
+			}
+		},
+		onDrive: 'Ice',
+		desc: "Changes Genesect to Genesect-Chill."
+	},
+	"dousedrive": {
+		id: "dousedrive",
+		name: "Douse Drive",
+		spritenum: 103,
+		fling: {
+			basePower: 70
+		},
+		onStart: function (pokemon) {
+			if (pokemon.species === 'Genesect') {
+				this.add('-item', pokemon, 'Douse Drive');
+				if (pokemon.formeChange('Genesect-Douse')) {
+					this.add('-formechange', pokemon, 'Genesect-Douse');
+				}
+			}
+		},
+		onBasePower: function(basePower, user, target, move) {
+			if (move.id === 'technoblast') {
+				return basePower * 1.1;
+			}
+		},
+		onDrive: 'Water',
+		desc: "Changes Genesect to Genesect-Douse."
+	},
+	"shockdrive": {
+		id: "shockdrive",
+		name: "Shock Drive",
+		spritenum: 103,
+		fling: {
+			basePower: 70
+		},
+		onStart: function (pokemon) {
+			if (pokemon.species === 'Genesect') {
+				this.add('-item', pokemon, 'Shock Drive');
+				if (pokemon.formeChange('Genesect-Shock')) {
+					this.add('-formechange', pokemon, 'Genesect-Shock');
+				}
+			}
+		},
+		onBasePower: function(basePower, user, target, move) {
+			if (move.id === 'technoblast') {
+				return basePower * 1.1;
+			}
+		},
+		onDrive: 'Electric',
+		desc: "Changes Genesect to Genesect-Shock."
+	},
+	"widelens": {
+		inherit: true,
+		onModifyMove: function(move, user, target) {
+			if (typeof move.accuracy === 'number') {
+				move.accuracy *= 1.3;
+			}
+		}
+	},
