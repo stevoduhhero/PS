69eed6d28452b50d68d00ce9e78a0856bd6eec67
Oiawesome <Oiawesome99@yahoo.com>
2013-07-30 13:34:16 -0700

    Create formats-data.js

---
+++ mods/pokecybertiers/formats-data.js
@@ -0,0 +1,118 @@
+exports.BattleFormatsData = {
+        chandelure: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        thundurus: {
+                inherit: true,
+                tier: 'OU'
+        },
+        tornadust: {
+                inherit: true,
+                tier: 'OU'
+        },        
+        landorus: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        tornadust: {
+                inherit: true,
+                tier: 'OU'
+        },        
+        victini: {
+                inherit: true,
+                tier: 'BL'
+        },
+        keldeor: {
+                inherit: true,
+                tier: 'BL'
+        },
+        hippowdon: {
+                inherit: true,
+                tier: 'BL'
+        },        
+        heracross: {
+                inherit: true,
+                tier: 'BL'
+        },
+        victini: {
+                inherit: true,
+                tier: 'BL'
+        },
+        cresselia: {
+                inherit: true,
+                tier: 'BL'
+        },
+        keldeo: {
+                inherit: true,
+                tier: 'BL'
+        },
+        reuniclus: {
+                inherit: true,
+                tier: 'BL'
+        },        
+        sawsbuck: {
+                inherit: true,
+                tier: 'UU'
+        },
+        ferroseed: {
+                inherit: true,
+                tier: 'UU'
+        },
+        entei: {
+                inherit: true,
+                tier: 'UU'
+        },
+        vaporeon: {
+                inherit: true,
+                tier: 'UU'
+        },
+        toxicroak: {
+                inherit: true,
+                tier: 'RU'
+        },
+        kabutops: {
+                inherit: true,
+                tier: 'BL2'
+        },
+        cobalion: {
+                inherit: true,
+                tier: 'RU'
+        },
+        ludicolo: {
+                inherit: true,
+                tier: 'BL3'
+        },        
+        scyther: {
+                inherit: true,
+                tier: 'NU'
+        },
+        hippopotas: {
+                inherit: true,
+                tier: 'NU'
+        },
+        politoed: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        unknown: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        womadam: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        womadamtrash: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        womadamsandy: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        swalot: {
+                inherit: true,
+                tier: 'LOU'
+        },          
+};
