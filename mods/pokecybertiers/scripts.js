e2ab692f80bdbb0277e1b4203913f1e66b2bcb44
Oiawesome <Oiawesome99@yahoo.com>
2013-07-30 13:34:45 -0700

    Create scripts.js

---
+++ mods/pokecybertiers/scripts.js
@@ -0,0 +1,6 @@
+exports.BattleScripts = {
+        init: function() {
+                for (var i in this.data.FormatsData)
+                        this.data.FormatsData[i].dreamWorldRelease = true;
+                }
+};
