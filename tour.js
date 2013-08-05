10eb780e5bad8a2bf79e474ce970abec2c1e1bbe
Slayer95 <ivojulca@hotmail.com>
2013-08-05 01:45:51 -0500

    Update tour.js

--- tour.js
+++ tour.js
@@ -86,8 +86,8 @@ exports.tour = function(t) {
 			}
 		},
 		userauth: function(user, room) {
-			if (!config.tourauth && user.can('mute') ) return true;
-			if (config.tourauth && config.groupsranking.indexOf(user.group) >= config.groupsranking.indexOf(config.tourauth) ) return true;
+			if (!config.tourauth && user.can('broadcast')) return true;
+			if (config.tourauth && config.groupsranking.indexOf(user.group) >= config.groupsranking.indexOf(config.tourauth)) return true;
 			if (room.auth && room.auth[user.userid]) return true;
 			return false;
 		},
